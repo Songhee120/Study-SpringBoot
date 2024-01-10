f066a8d9fb3b938c3948a19708f66a6187a55300
Songhee Lee <clasonghee@gmail.com>
2024-01-10 18:26:15 +0900

    add config file

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 // Anotation : Controller, Service, Repository
 
@@ -16,5 +17,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 }
